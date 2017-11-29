681270e571434eec7fc906b63d1477b4985ec244
Sofia101 <33931647+Sofia101@users.noreply.github.com>
2017-11-29 22:32:20 +0800

    PetPetAnimal認養平台後台管理第一版 其他檔案
    認養平台後台管理第一版 其他檔案

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PetPetAnimal認養平台.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+using PetPetAnimal認養平台;
+
+[assembly: OwinStartupAttribute(typeof(PetPetAnimal認養平台.Startup))]
+namespace PetPetAnimal認養平台
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
